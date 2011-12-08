d3d9fa5c38fab1e5660a6173afad2e031832d94a
Ducatel <hannibal@hannibal-PC.(none)>
2011-12-08 17:32:18 +0100

    Ajout d'une trace d'execution de l'ordonnanceur

--- Frontales/FrontalCompilation/src/Main.py
+++ Frontales/FrontalCompilation/src/Main.py
@@ -41,5 +41,25 @@ else:
 print(ordo)
 
 
+################################## resultat d'execution ##################
+"""
+Nombre de serveurs gere l'ordonnanceur: 3
+Nombre de serveurs disponible: 3
+Position dans l'ordonnanceur: 0    adresse IP: 192.168.1.2    numero du port: 4242
+Position dans l'ordonnanceur: 1    adresse IP: 192.168.1.3    numero du port: 4343
+Position dans l'ordonnanceur: 2    adresse IP: 192.168.1.4    numero du port: 4444
 
+('192.168.1.2', '4242')
+Nombre de serveurs gere l'ordonnanceur: 3
+Nombre de serveurs disponible: 2
+Position dans l'ordonnanceur: 0    adresse IP: 192.168.1.3    numero du port: 4343
+Position dans l'ordonnanceur: 1    adresse IP: 192.168.1.4    numero du port: 4444
+
+Ajout sans probleme
+Nombre de serveurs gere l'ordonnanceur: 3
+Nombre de serveurs disponible: 3
+Position dans l'ordonnanceur: 0    adresse IP: 192.168.1.3    numero du port: 4343
+Position dans l'ordonnanceur: 1    adresse IP: 192.168.1.4    numero du port: 4444
+Position dans l'ordonnanceur: 2    adresse IP: 192.168.1.2    numero du port: 4242
+"""
 
