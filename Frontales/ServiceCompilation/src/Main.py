af65d34e97cf58c6140ae385f97c63bca6f00864
David <hannibal@hannibal-laptop.(none)>
2012-01-19 16:05:56 +0100

    Ajout du debut du service de compilation

---
+++ Frontales/ServiceCompilation/src/Main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*-coding:utf-8 -*
+'''
+Created on 19 janv. 2012
+
+@author: David Ducatel
+'''
+import ServiceCompilation
+
+ServiceCompilation=ServiceCompilation.ServiceCompilation('172.16.21.186',12800)
+ServiceCompilation.lanceServeur()
