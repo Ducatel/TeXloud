01259ca63b29b02c685f71be28fb00a0d20d63a8
Meva Rakotondratsima <meva@Baal.(none)>
2012-01-26 16:50:53 +0100

    Mise à jour dans les requêtes

--- Connectors/svnConnector/connector/SvnDataSocket.py
+++ Connectors/svnConnector/connector/SvnDataSocket.py
@@ -18,16 +18,23 @@ class SvnDataSocket(DataSocket):
     def __init__(self, port, address):
         super(self.__class__,self).__init__(port, address)
             
-    def getProject(self, path, username, password, client):
-        super(SvnDataSocket, self).getProject(path, username, password, client)
+    def getProject(self, path, username, password, httpPort, client):
+        super(SvnDataSocket, self).getProject(path, username, password, httpPort, client)
+        
+        working_copy_dir = False
         
         if not '..' in path:
             print path
             # connection = SvnConnector.SvnConnector('svn://87.98.144.200/test_svn', 'meva', 'plop', path)
             connection = SvnConnector.SvnConnector(path, username, password)
             self.save_user_conf(connection.get_public_dir_name(), username, password, connection.get_url())
+            
+            working_copy_dir = connection.get_public_dir_name()
         
-    def deleteProject(self, path, client):
+        request = '{"projectName" : "' + connection.get_url() + '", "workingCopyDir":"' + connection.get_public_dir_name() + '"}'
+        super(SvnDataSocket, self).sendAsciiMessage(request, self._webserver_ip, httpPort)
+        
+    def deleteProject(self, path, httpPort, client):
         super(SvnDataSocket, self).deleteProject(path, client)
         print path + ' deleted'
         
@@ -36,10 +43,10 @@ class SvnDataSocket(DataSocket):
     #    return NotImplemented
     
     @abstractmethod
-    def compileProject(self, rootFile, path, compilerServerIp, compilerServerPort, client):
+    def compileProject(self, rootFile, path, compilerServerIp, compilerServerPort, httpPort, client):
         return NotImplemented
    
-    def deleteFile(self, path, filename, client):
+    def deleteFile(self, path, filename, httpPort, client):
         if not '..' in path and not '..' in filename:
             super(SvnDataSocket, self).deleteFile(path, filename, client)
             
@@ -49,16 +56,16 @@ class SvnDataSocket(DataSocket):
             connection.commit()
             print path + '/' + filename + ' deleted'
             
-    def createFile(self, path, filename, file_content, client):
-        super(SvnDataSocket, self).createFile(path, filename, file_content, client)
+    def createFile(self, path, filename, file_content, httpPort, client):
+        super(SvnDataSocket, self).createFile(path, filename, file_content, httpPort, client)
         connection = SvnConnector.SvnConnector(self.getUserProperty(path, 'repo'), self.getUserProperty(path, 'username'), 
                                                    self.getUserProperty(path, 'password'), path, False)
         print 'commit to ' + self.getUserProperty(path, 'repo') + ', user -> ' + self.getUserProperty(path, 'username') + ', pwd -> ' + self.getUserProperty(path, 'password')
         connection.full_commit()
         print 'file commited'
     
-    def sync(self, path, currentFile, files, client):
-        super(SvnDataSocket, self).sync(path, currentFile, files, client)
+    def sync(self, path, currentFile, files, httpPort, client):
+        super(SvnDataSocket, self).sync(path, currentFile, files, httpPort, client)
         
         if not '..' in path:
             connection = SvnConnector.SvnConnector(self.getUserProperty(path, 'repo'), self.getUserProperty(path, 'username'), 
