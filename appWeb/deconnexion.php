bfaf010b4fa41d22c9ba13effc454f34ca5f5f78
Ducatel <hannibal@hannibal-PC.(none)>
2012-03-01 12:58:31 +0100

    nettoyage

--- appWeb/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-session_start();
-unset($_SESSION);
-session_destroy();
-header ('Location: Acceuil.html');
- ?>
--- appWeb/texloud.js
+++
@@ -1,6 +0,0 @@
-
-$(function() {
-	$(".lined").linedtextarea(
-		{selectedLine: 1}
-	);
-});
